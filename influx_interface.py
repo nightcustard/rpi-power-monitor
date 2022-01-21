53721836ba1da711926d3313bc568b13e99c272e
Nightcustard <nightcustard@gmail.com>
2022-01-21 22:34:01 +0000

    Update influx_interface.py

--- influx_interface.py
+++ influx_interface.py
@@ -78,7 +78,7 @@ class Point():
             self.p_type  = p_type
  
     def to_dict(self):
-        if self.p_type == 'home_residual_load
+        if self.p_type == 'home_residual_load':
             data = {
                 "measurement": 'residual_load',
                 "fields" : {
@@ -164,7 +164,7 @@ def init_db():
     
     
 
-residual_load
+
 def close_db():
     client.close()
 
@@ -195,7 +195,7 @@ def write_to_influx(solar_power_values, residual_load_values, net_power_values, 
     ct4_avg_pf = sum(ct4_dict['pf']) / length
     ct5_avg_power = sum(ct5_dict['power']) / length
     ct5_avg_current = sum(ct5_dict['current']) / length
-    residual_loadf = sum(ctresidual_loadf']) / length
+    ct5_avg_pf = sum(ct5_dict['pf']) / length
     avg_voltage = sum(voltages) / length
 
     # Create Points
@@ -207,7 +207,7 @@ def write_to_influx(solar_power_values, residual_load_values, net_power_values, 
     ct2 = Point('ct', power=ct2_avg_power, current=ct2_avg_current, pf=ct2_avg_pf, tariff=current_tariff, time=poll_time, num=2)
     ct3 = Point('ct', power=ct3_avg_power, current=ct3_avg_current, pf=ct3_avg_pf, tariff=current_tariff, time=poll_time, num=3)
     ct4 = Point('ct', power=ct4_avg_power, current=ct4_avg_current, pf=ct4_avg_pf, tariff=current_tariff, time=poll_time, num=4)
-    ct5 residual_loadct', power=ct5_avg_power, current=ct5_avg_current, pf=ct5_avg_pf, tariff=current_tariff, time=poll_time, num=5)
+    ct5 = Point('ct', power=ct5_avg_power, current=ct5_avg_current, pf=ct5_avg_pf, time=poll_time, num=5)
     v = Point('voltage', voltage=avg_voltage, v_input=0, time=poll_time)
 
     points = [
